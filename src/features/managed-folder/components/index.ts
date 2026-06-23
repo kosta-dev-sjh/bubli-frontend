afa3b173da9efd720903eb99e38d3599351141df
Song991123 <sjh11235678@gmail.com>
2026-06-23 11:53:44 +0900

    chore: export storage sync policy panel

--- src/features/managed-folder/components/index.ts
+++ src/features/managed-folder/components/index.ts
@@ -3,3 +3,4 @@ export * from "./managed-folder-index-panel";
 export * from "./managed-folder-policy-panel";
 export * from "./managed-folder-s3-handoff-panel";
 export * from "./managed-folder-sync-panel";
+export * from "./storage-sync-policy-panel";
