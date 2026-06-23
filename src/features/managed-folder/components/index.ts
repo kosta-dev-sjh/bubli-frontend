7b7eef657c5fba3fc81a0698459dfa6c7781582e
Song991123 <sjh11235678@gmail.com>
2026-06-23 11:35:58 +0900

    chore: update managed folder component exports

--- src/features/managed-folder/components/index.ts
+++ src/features/managed-folder/components/index.ts
@@ -1,2 +1,5 @@
+export * from "./managed-folder-change-review";
 export * from "./managed-folder-index-panel";
+export * from "./managed-folder-policy-panel";
+export * from "./managed-folder-s3-handoff-panel";
 export * from "./managed-folder-sync-panel";
