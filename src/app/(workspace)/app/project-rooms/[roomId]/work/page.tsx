69281ac31be8f0e76535c9e31b8a0e312bc4b48f
Song991123 <sjh11235678@gmail.com>
2026-06-25 23:16:20 +0900

    feat: compose project room work route

--- src/app/(workspace)/app/project-rooms/[roomId]/work/page.tsx
+++ src/app/(workspace)/app/project-rooms/[roomId]/work/page.tsx
@@ -1,5 +1,6 @@
 import { PageHeading } from "@/components/ui/page-heading";
-import { WbsTodoBoard } from "@/features/wbs/components/wbs-todo-board";
+import { TodoDetailPanel } from "@/features/todo/components";
+import { WbsTodoBoard, WbsTodoLinkagePanel } from "@/features/wbs/components";
 
 type ProjectRoomWorkPageProps = {
   params: Promise<{
@@ -17,7 +18,9 @@ export default async function ProjectRoomWorkPage({ params }: ProjectRoomWorkPag
         description={`프로젝트룸 ${roomId}의 WBS, TODO, 칸반, 타임라인을 같은 작업 기준으로 봅니다.`}
       />
       <div className="page-grid">
+        <WbsTodoLinkagePanel />
         <WbsTodoBoard />
+        <TodoDetailPanel />
       </div>
     </>
   );
---
+++ src/features/wbs/components/index.ts
@@ -0,0 +1,2 @@
+export * from "./wbs-todo-board";
+export * from "./wbs-todo-linkage-panel";
